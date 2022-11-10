494ed2e5e437ba06103f17f940763e94d18b347d
seo0033 <tkrhk9511@gmail.com>
2022-11-10 17:33:38 +0900

    20221110

--- app.js
+++ app.js
@@ -6,4 +6,37 @@ const myName = "seo0";
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
-console.log("hello " + myName); 
+console.log("hello " + myName);
+
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"]
+
+//배열 받기(확인)
+console.log(daysOfWeek);
+
+//배열이름[번호] / 번호: 0부터
+console.log(daysOfWeek[2])
+
+//배열 추가
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+//Object(객체)
+const player = {
+    name: "seo0",
+    points: 10,
+    fat: true,
+};
+
+console.log(player);
+console.log(player.name);
+
+//Object에 property추가
+player.color = "purple";
+
+//property 업데이트
+player.points = 15;
+
+console.log(player)
+
